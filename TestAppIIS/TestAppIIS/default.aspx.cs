34e94b28f676acfdcbc44d02ab14e48123d8ce5e
Joyal G <joyalgs@gmail.com>
2023-01-16 21:30:24 -0400

    Initial commit

---
+++ TestAppIIS/TestAppIIS/default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Security.Principal;
+
+namespace TestAppIIS
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            resWrite("GetCurrent user: ");
+            resWrite(WindowsIdentity.GetCurrent().Name + "<br>");
+
+            resWrite("User IsAuthenticated : ");
+            resWrite(User.Identity.IsAuthenticated.ToString() + "<br>");
+
+            resWrite("User AuthenticationType : ");
+            resWrite(User.Identity.AuthenticationType.ToString() + "<br>");
+
+            resWrite("User Name : ");
+            resWrite(User.Identity.Name.ToString() + "<br>");
+
+        }
+
+        private void resWrite(string msg)
+        {
+            Response.Write(msg + "<br>");
+        }
+    }
+}
---
+++ TestAppIIS/TestAppIIS/readme.md
@@ -0,0 +1,7 @@
+﻿
+
+https://stackoverflow.com/questions/12676002/aspnet-regiis-exe-ir-does-not-work-on-windows-8
+
+Run the following command, which will install and register ASP.NET 4.5 in IIS:
+
+dism /online /enable-feature /featurename:IIS-ASPNET45 /all
